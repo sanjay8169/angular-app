5f31455ff9cb056b69190d329244f1cd983afa37
sanjay8169 <yadavsam975@gmail.com>
2024-06-18 00:26:39 +0530

    routed the components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { ProductListComponent } from '../product-list/product-list.component';
+import { ListComponent } from '../list/list.component';
+import { ProfileDetailsComponent } from '../profile-details/profile-details.component';
+import { HomeComponent } from '../home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,RouterLink,HomeComponent,ProductListComponent,ListComponent,ProfileDetailsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { HomeComponent } from '../home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path : "",component : HomeComponent},
+    {path : "list",loadComponent: () => import('../list/list.component').then(a=> a.ListComponent)},
+    {path : "profile-builder", loadComponent : () => import('../profile-details/profile-details.component').then(a => a.ProfileDetailsComponent)},
+    {path : "products", loadComponent : () => import('../product-list/product-list.component').then(a => a.ProductListComponent)}
+];
